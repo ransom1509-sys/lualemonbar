c84a2e172eddfab6f553532175053db18e16423c
ransom1509 <js@porcaro.localdomain>
2026-03-02 09:57:36 +0100

    Invoke lemonbar with io.popen() 	geändert:       lemonbar-lua.lua

--- lemonbar-lua.lua
+++ lemonbar-lua.lua
@@ -7,11 +7,17 @@ local posix = require("posix")
 -- local socket = require("socket")
 local sleep = posix.sleep
 local n     = 1
+local cmd  = "lemonbar -g 1056+0+0 -p -u 2 -f \'Cousine for Powerline:pixelsize=14\' -f \'Typicons:pixelsiz=14\' -f \'Symbols Nerd Font Mono:pixelsize=14\' -f  \'Cousine for Powerline:style=Bold:pixelsize=14\' -B#ff1a1b26 | sh"
 
--- +++++++++++ Stand-alone ++++++++++++++++
+-- cmd = "lemonbar -p"
+local pipe_out = assert(io.popen(cmd, "w"))
+local text
 
 lemonbar.init(mybar)
+
 while true do
-  print(lemonbar.show(mybar))
+  text = lemonbar.show(mybar) .. "\n"
+  pipe_out:write(text)
+  pipe_out:flush()
   sleep(n)
 end
