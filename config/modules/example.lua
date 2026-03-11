8499a9a6a9cc0c5295c693f2dfdb9fef120012a8
ransom1509 <js@porcaro.localdomain>
2026-03-11 12:31:26 +0100

    Added flly commented example.lua module template.

--- config/modules/example.lua
+++ config/modules/example.lua
@@ -1,24 +1,34 @@
-local  testmod = {}
-function testmod.setup(bar)
-  bar["testmod"] = {
-    fgc1    = "%{F#b6c0e9}",
-    bgc     = bar.colors.bgc1,
-    sfg     = "",
-    sbg     = "",
-    iv      = 1,
-    secs    = 0;
-    show    = "",
-    sep     = "",
-    fmt     = "",
-    icon    = "Count:",
+-- Template for simple modules.
+-- Edit config.ini and add "example" to [settings] -> modules to see it running.
+-- Add header [example] to config.ini, place some field names (e.g. fgc1 below the
+-- header and play around with the module.
+local  example = {}
+function example.setup(bar)
+-- Anything availlable for config.ini must be decalared here
+  bar["example"] = {
+    fgc1    = "%{F#b6c0e9}",    -- Text color See config.ini for availlable colors.
+    bgc     = bar.colors.bgc1,  -- Background colo. 
+    sfg     = "",               -- Separatot forground, color.
+    sbg     = "",               -- Separator background, color.
+    iv      = 1,                -- Update intervall, required.
+    secs    = 0;                -- Internal counter, required.
+    show    = "",               -- What the module returns, required.
+    sep     = "",               -- Character or glyph used as Separator.
+    icon    = "Count:",         -- Label or glyph for the modul.
+    fmt     = "",               -- Alignmen tleft, center, right), required
+                                -- Impacts all following modules.
+
+    -- implement update() as coroutine
+    -- bar.show() calls coroutine.resume(bar[<module>][update])
     update = coroutine.create(function()
-      local c1    = bar.testmod.fgc1
-      local bc    = bar.testmod.bgc
-      local icon  = bar.testmod.icon
+      local c1    = bar.example.fgc1
+      local bc    = bar.example.bgc
+      local icon  = bar.example.icon
       local i     = 1
 
       while true do
-        bar.testmod.show = string.format("%s%s%s %s ", bc, c1, icon, tostring(i))
+        -- The actual modul code
+        bar.example.show = string.format("%s%s%s %s ", bc, c1, icon, tostring(i))
         if i >= 10 then
           i = 1
         else
@@ -28,12 +38,14 @@ function testmod.setup(bar)
       end
     end),
 
+    -- Any required initialisatiom
+    -- Can be empty, but must be present
     init = function ()
-      local sf        = bar.testmod.sfg
-      local sb        = bar.testmod.sbg
-      local symbol    = bar.testmod.sep
+      local sf        = bar.example.sfg
+      local sb        = bar.example.sbg
+      local symbol    = bar.example.sep
       local sep       = bar.tools.seperator(symbol, sf, sb, 3 )
-      bar.testmod.sep = sep
+      bar.example.sep = sep
     end,
 
   }
@@ -42,4 +54,4 @@ function testmod.setup(bar)
 
   end
 
-return testmod
+return example
